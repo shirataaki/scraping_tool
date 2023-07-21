3dce204b194ba752c31ced503c20261b91d710ae
久保佳子 <kubo@kuboyoshikonoMacBook-Air.local>
2023-07-21 17:01:48 +0900

    h1タグの内容を出力

--- new_scraping.py
+++ new_scraping.py
@@ -73,6 +73,7 @@ def get_links_recursive(url, num):
         relative_url = link.get('href', '')
         if '#' in relative_url:
             continue
+        
         title = link.string
         span = link.find('span')
         if span is not None:
@@ -83,6 +84,10 @@ def get_links_recursive(url, num):
         # HTMLから抽出した相対URLを絶対URLに変換し、正規化する
         full_url = normalize_url( urljoin(url, relative_url) )
         if main_url in full_url:
+            h1_content = '' # h1タグの内容を保存する変数を初期化
+            thumbnail_url = ""  # サムネイルURLの変数を初期化
+            page_description = ''  # ページの説明文を保存する変数を初期化
+
             if full_url in visited_links:
                 continue
 
@@ -102,6 +107,24 @@ def get_links_recursive(url, num):
                     soup_link = BeautifulSoup(reqs_link.content, 'lxml')
                     remove_unnecessary_elements(soup_link)  # リンク先のHTMLからも不要な要素を削除
                     html = str(soup_link.main) # HTMLのmain部分を抽出
+
+                    # h1タグの内容を取得
+                    h1_tag = soup_link.find('h1') # h1タグを探す
+                    if h1_tag is not None: # h1タグが見つかった場合
+                        h1_content = h1_tag.get_text(strip=True) # テキストを取得
+                    
+                    #og:imageのURLを取得
+                    og_image = soup_link.find('meta', property='og:image')
+                    if og_image and og_image.get('content'):
+                        thumbnail_url = og_image.get('content')
+                    else:
+                        thumbnail_url = ""  # og:imageが見つからない場合は空文字列にする
+                    
+                    # metaタグからdescriptionを取得
+                    meta_description_tag = soup_link.find('meta', attrs={'name': 'description'})
+                    if meta_description_tag is not None:
+                        page_description = meta_description_tag.get('content', '')
+                    
                 except requests.exceptions.Timeout:
                     print(f"{full_url} へのHTML取得のリクエストがタイムアウトしました")
                     html = "error"
@@ -109,7 +132,7 @@ def get_links_recursive(url, num):
                     print(f"{full_url} へのHTML取得のリクエストに失敗しました")
                     html = ""
             
-            data.append([title, full_url, html, fetch_date])
+            data.append([title, full_url, h1_content, page_description, html, thumbnail_url, fetch_date])
             num += 1
             print(f"{num}個目のURLを発見: {full_url}")
             
@@ -137,5 +160,5 @@ def explore_links_until_exhausted(url):
 data = explore_links_until_exhausted(url3)
 
 # DataFrameを作成し、CSVとして出力
-df = pd.DataFrame(data, columns=['Title', 'URL', 'HTML', '取得日'])
-df.to_csv('digital_03.csv', index=False, encoding='utf-8')
+df = pd.DataFrame(data, columns=['Title', 'URL', 'h1', 'ページ概要説明テキスト', 'HTML', 'サムネイル画像URL', '取得日'])
+df.to_csv('digital_05.csv', index=False, encoding='utf-8')
